91b7e9ca0b6459e861f1be1c895af3c44aad0588
bdb252 <hanyilee@naver.com>
2025-11-25 14:11:25 +0900

    hani add

--- src/main/java/com/ccdweb/springboot/jpa/meallog/MealLogRepository.java
+++ src/main/java/com/ccdweb/springboot/jpa/meallog/MealLogRepository.java
@@ -1,6 +1,5 @@
 package com.ccdweb.springboot.jpa.meallog;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/com/ccdweb/springboot/jpa/userlog/UserLogEntity.java
+++ src/main/java/com/ccdweb/springboot/jpa/userlog/UserLogEntity.java
@@ -16,7 +16,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
